6468ae40e848bf665aeef8a0142901befad7ab7c
Dmytro Zhluktenko <d1mnewz@gmail.com>
2018-11-26 22:46:32 +0200

    initial setup which will be refactored during the challenge

--- RoverDojo/Program.cs
+++ RoverDojo/Program.cs
@@ -2,11 +2,65 @@
 
 namespace RoverDojo
 {
-    class Program
+    public class Rover
     {
-        static void Main(string[] args)
+        public enum RoverFacing
         {
-            Console.WriteLine("Hello World!");
+            North,
+            East,
+            South,
+            West
+        }
+
+        public static void Main()
+        {
+            var roverPositionX = 0;
+            var roverPositionY = 0;
+            var roverFacing = RoverFacing.North;
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command != "L" && command != "R" && command != "F")
+                    throw new Exception("invalid command");
+
+                switch (command)
+                {
+                    case "L":
+                        roverFacing = roverFacing == RoverFacing.North
+                            ? RoverFacing.West
+                            : (RoverFacing) ((int) roverFacing - 1);
+                        Console.WriteLine($"Rover is now at {roverPositionX}, {roverPositionY} - facing {roverFacing}");
+                        break;
+                    case "R":
+                        roverFacing = roverFacing == RoverFacing.West
+                            ? RoverFacing.North
+                            : (RoverFacing) ((int) roverFacing + 1);
+                        Console.WriteLine($"Rover is now at {roverPositionX}, {roverPositionY} - facing {roverFacing}");
+                        break;
+                    case "F":
+                        switch (roverFacing)
+                        {
+                            case RoverFacing.North:
+                                roverPositionX++;
+                                break;
+                            case RoverFacing.East:
+                                roverPositionY++;
+                                break;
+                            case RoverFacing.South:
+                                roverPositionX--;
+                                break;
+                            case RoverFacing.West:
+                                roverPositionY--;
+                                break;
+                        }
+
+                        Console.WriteLine($"Rover is now at {roverPositionX}, {roverPositionY} - facing {roverFacing}");
+                        break;
+                    default:
+                        throw new Exception("invalid command");
+                }
+            }
         }
     }
 }
